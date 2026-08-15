30304602b7f2f1c755872e15523ccfeef22cfac4
SarkarAbhi01 <abhialshpathakforbusinessid@gmail.com>
2026-08-15 22:40:55 +0530

    fix postgres ssl connection

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,19 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-
+const isProduction = process.env.NODE_ENV === 'production';
 const pool = new Pool({
   host: process.env.PGHOST || 'localhost',
   port: process.env.PGPORT || 5432,
   user: process.env.PGUSER || 'postgres',
   password: process.env.PGPASSWORD || 'postgres',
   database: process.env.PGDATABASE || 'sound_dj_booking',
+
+   // Render PostgreSQL requires SSL
+  ssl: isProduction
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
 });
 
 pool.on('error', (err) => {
